e88f096ebda6e2059268e7051b271d1fa3896ba3
Ruben Vargas <ruben.vp8510@gmail.com>
2020-10-23 11:15:12 -0500

    Comment on PR
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-import {comment} from './github'
+import {doComment} from './github'
+import {
+  parseResults,
+  compareDeltasWithThreshold,
+  renderMarkdownTable
+} from './benchstats'
+
+import {getInput, info} from '@actions/core'
 
 async function run(): Promise<void> {
-  await comment('prueba')
+  const bench_file = getInput('benchstat_file')
+  const threshold = getInput('delta_threshold')
+  const metrics = getInput('metrics').split(',')
+
+  const suites = await parseResults(bench_file)
+
+  if (compareDeltasWithThreshold(suites, parseFloat(threshold), metrics)) {
+    info(`Building comment.`)
+    const lines = [
+      ':warning: **Performance Alert** :warning:',
+      'Possible performance regression was detected'
+    ]
+    const resultsTable = renderMarkdownTable(suites)
+    lines.push(resultsTable)
+    const comment: string = lines.join('\n')
+    info(comment)
+    await doComment(comment)
+  }
 }
+
 run()
